e89bf626771da8fb6c0a488f690219ebdee4dc19
Sofiane OUAFIR <sofiane.ouafir@live.fr>
2017-05-02 22:40:13 +0200

    Update education-list.component.spec.js

--- app/education-list/education-list.component.spec.js
+++ app/education-list/education-list.component.spec.js
@@ -10,7 +10,7 @@ describe('educationList', function(){
 
     beforeEach(inject(function($componentController, _$httpBackend_){
       $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('http://68.66.240.35/admin-cv/education/list?lang=en')
+      $httpBackend.expectGET('http://68.66.240.35/admin-cv/education/list?lang=e')
                   .respond([{'class':'cv.Education','id':1,'description':'lala','enddate':'2017-04-01T22:00:00Z','place':'la','startdate':'2017-08-06T22:00:00Z','title':'education1','user':{'class':'cv.User','id':1}},{'class':'cv.Education','id':2,'description':'lala','enddate':'2017-04-02T12:13:48Z','place':'la','startdate':'2017-04-02T12:13:48Z','title':'education2','user':{'class':'cv.User','id':1}},{'class':'cv.Education','id':3,'description':'ddd','enddate':'2017-04-06T22:00:00Z','place':'ddd','startdate':'2017-08-05T22:00:00Z','title':'coteaux','user':{'class':'cv.User','id':1}}]);
       ctrl = $componentController('educationList');
     }));
